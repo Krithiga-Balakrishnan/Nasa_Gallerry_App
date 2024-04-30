664f6ab14c82cff4c08a57993ebe8a86d2b338bf
Krithiga Balakrishnan <krithigadb96@gmail.com>
2024-04-30 13:58:03 +0530

    error rectified

--- src/components/DateInput.js
+++ src/components/DateInput.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import DatePicker from "react-datepicker";
 import { getYear, getMonth } from "date-fns";
-import '../css/dateinput.css';
+import '../css/DateInput.css';
 import "react-datepicker/dist/react-datepicker.css";
 
 function range(start, end, step = 1) {
